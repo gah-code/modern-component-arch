afa3e5ff76b9e2971577028bd7fd048500cac8cb
Gilbert Haro <gilbertharocode@gmail.com>
2024-03-07 20:43:38 -0800

    set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styled from 'styled-components';
 
-function App() {
+function FrequentlyAskedQuestion({ question, answer }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <details className='faq'>
+      <summary>{question}</summary>
+      <div className='answer'>{answer}</div>
+    </details>
   );
 }
+
+const App = () => (
+  <FrequentlyAskedQuestion
+    question='What does “CSS” stand for?'
+    answer='Cool Styling Strategy'
+  />
+);
 
 export default App;
